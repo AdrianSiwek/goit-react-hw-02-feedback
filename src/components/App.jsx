f1531baa27a6203c81ee7edcd33accc8bb860b3d
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-03-20 23:31:36 +0100

    update and create app feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Section from './Section/Section';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+
+export class App extends Component {
+  state = { 
+    good: 0,
+    neutral: 0,
+    bad: 0
+  } 
+  
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return Math.round((good *100)/total);
+  }
+
+  render() { 
+    const { good, neutral, bad } = this.state
+    const total = good +neutral + bad
+    return (
+      <>
+        <Section title="Please leave feedback" />
+        
+        {total > 0 ? (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback}
+              positivePercentage={this.countPositiveFeedback}
+            />
+          </Section>
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </>
+    );
+  }
+}
+ 
+
